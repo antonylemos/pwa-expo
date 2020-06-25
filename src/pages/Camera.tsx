53ede8fa00e2f0cf387787d1161b0d0400c05198
Antony Lemos <lemosantony@gmail.com>
2020-06-25 10:44:01 -0300

    insert camera

--- src/pages/Camera.tsx
+++ src/pages/Camera.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { View } from 'react-native';
+import { Camera as ExpoCamera } from 'expo-camera';
 
 const Camera: React.FC = () => {
-  return <View />
+  return (
+    <View style={{ flex: 1 }}>
+      <ExpoCamera style={{ flex: 1 }} />
+    </View>
+  );
 }
 
 export default Camera;
